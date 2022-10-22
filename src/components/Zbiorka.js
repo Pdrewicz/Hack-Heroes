fbc390d131df8b30a67c5fa0ec1e2b58b51c9a36
Pdrewicz <106173218+Pdrewicz@users.noreply.github.com>
2022-10-22 13:00:44 +0200

    a

--- src/components/Zbiorka.js
+++ src/components/Zbiorka.js
@@ -1,6 +1,6 @@
 export const Zbiorka = ({ image, title, text }) => {
   return <div id="zbiorka">
-    <img src={image} alt=".a." />
+    <img src={image} alt="..." />
     <div>
       <h1>{title}</h1>
       <p>{text}</p>
