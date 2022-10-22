78a3b37dc090c4decb0c9a8661b35fbca6f1c56f
Pdrewicz <106173218+Pdrewicz@users.noreply.github.com>
2022-10-22 12:58:23 +0200

    a

--- src/components/Zbiorki.js
+++ src/components/Zbiorki.js
@@ -2,7 +2,7 @@ import { Zbiorka } from "./Zbiorka";
 
 export const Zbiorki = () => {
   return <div id="zbiorki">
-    <Zbiorka image={require("../Images/moon.png")} title={"TEST"} text={"test test tes"}/>
+    <Zbiorka image={require("../Images/moon.png")} title={"TEST"} text={"test test test"}/>
     <Zbiorka image={require("../Images/moon.png")} title={"TEST"} text={"test test test"}/>
     <Zbiorka image={require("../Images/moon.png")} title={"TEST"} text={"test test test"}/>
     <Zbiorka image={require("../Images/moon.png")} title={"TEST"} text={"test test test"}/>
